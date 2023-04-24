3f40b1ac58428eb8c958425508c2cb69d4735c57
IvanKazangi12345 <131653342+IvanKazangi12345@users.noreply.github.com>
2023-04-24 11:56:00 +0300

    Add files via upload

---
+++ task-02.js
@@ -0,0 +1,15 @@
+const ingredientsList = document.getElementById('ingredients');
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredient;
+  ingredientsList.appendChild(li);
+});
