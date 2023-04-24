9b4b3a34951b5375730b8d1ba69877e273db4744
IvanKazangi12345 <131653342+IvanKazangi12345@users.noreply.github.com>
2023-04-24 15:48:23 +0300

    Update task-03.js

--- task-03.js
+++ task-03.js
@@ -1,23 +1,22 @@
 const images = [
-{
-url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-alt: "White and Black Long Fur Cat",
-},
-{
-url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-},
-{
-url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-alt: "Group of Horses Running",
-},
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
 ];
 
-const galleryList = document.querySelector('.gallery');
-const createGalleryItem = (url, alt) => {
-return <li class="gallery__item"> <img src="${url}" alt="${alt}" class="gallery__image"> </li> ;
-};
+const ul = document.querySelector(".gallery");
 
-const galleryItems = images.map(image => createGalleryItem(image.url, image.alt));
+const createGalleryMarkup = images.map(({ url, alt }) => {
+  return `<li><img src="${url}" alt="${alt}" /></li>`;
+}).join("");
 
-galleryList.insertAdjacentHTML('beforeend', galleryItems.join(''));
+ul.insertAdjacentHTML("beforeend", createGalleryMarkup);
