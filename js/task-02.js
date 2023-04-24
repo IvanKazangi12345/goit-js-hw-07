ddbd6a8000143ce5e43ae1ed7ddcd7f58c99ba66
IvanKazangi12345 <131653342+IvanKazangi12345@users.noreply.github.com>
2023-04-24 21:39:50 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,1 @@
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
 
-const ul = document.getElementById("ingredients");
-
-const result = ingredients.map((ingredient) => {
-  const li = document.createElement("li");
-  li.innerText = ingredient;
-  li.className = "item";
-  return(li);
-});
-ul.append(...result)
