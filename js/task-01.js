d9001c3fd1c766e8eaa280bcddecad77e157d734
IvanKazangi12345 <131653342+IvanKazangi12345@users.noreply.github.com>
2023-04-24 21:12:02 +0300

    Add files via upload

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesList = document.getElementById('categories');
+const categoryItems = categoriesList.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoryItems.length}`);
+categoryItems.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const elements = item.querySelectorAll('li');
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elements.length}`);
+});
